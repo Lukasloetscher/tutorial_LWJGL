edb5e60d07ba04fcb9d28cd9dae7bd9790288e4e
Lukas Loetscher <Lukas@Loetscher.swiss>
2025-05-20 18:58:45 +0200

    added base structure

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import myEngine.Window;
+
+public class Main {
+    public static void main(String[] args) {
+        Window window = Window.get();
+        window.run();
+    }
+
+
+}
---
+++ src/main/java/myEngine/Window.java
@@ -0,0 +1,15 @@
+package myEngine;
+
+public class Window {
+    private Window(){
+
+    }
+    public static Window get(){
+        return new Window();
+    }
+
+    public void run(){
+
+    }
+
+}
