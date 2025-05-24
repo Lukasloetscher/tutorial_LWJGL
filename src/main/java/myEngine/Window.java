37796b87e31437c6e5a6a15e8e295303b5d24063
Lukas Loetscher <Lukas@Loetscher.swiss>
2025-05-24 16:33:21 +0200

    added content to make window visible

--- src/main/java/myEngine/Window.java
+++ src/main/java/myEngine/Window.java
@@ -1,10 +1,15 @@
 package myEngine;
 
-import org.lwjgl.Version;
-import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
 
+import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
-
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
 public class Window {
     private static final Window myWindow = new Window();
@@ -26,6 +31,7 @@ public class Window {
     public void run(){
         System.out.println("Hello World" + Version.getVersion());
         init();
+        loop();
     }
 
     public void init(){
@@ -44,13 +50,49 @@ public class Window {
         glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
 
         //create the Window
-        glfwWindow = glfwCreateWindow(width, height, title, 0, 0);
+        glfwWindow = glfwCreateWindow(width, height, title, NULL, NULL); //NULL ?
+        if (glfwWindow == NULL){
+            throw new RuntimeException("Failed to create the GLFW window");
+        }
+        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+        glfwSetKeyCallback(glfwWindow, (window, key, scancode, action, mods) -> {
+            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+                glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+        });
 
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+        // Enable v-sync
+        glfwSwapInterval(1);
 
+        // Make the window visible
+        glfwShowWindow(glfwWindow);
 
-
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
 
 
     }
+    public void loop(){
+        // Set the clear color
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+
+        // Run the rendering loop until the user has attempted to close
+        // the window or has pressed the ESCAPE key.
+        while ( !glfwWindowShouldClose(glfwWindow) ) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+            glfwSwapBuffers(glfwWindow); // swap the color buffers
+
+            // Poll for window events. The key callback above will only be
+            // invoked during this call.
+            glfwPollEvents();
+        }
+    }
+
 
 }
