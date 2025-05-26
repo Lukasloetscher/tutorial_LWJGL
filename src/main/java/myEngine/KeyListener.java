0b7441504ac26e0178c3ff4f307f516c144ac6e4
Lukas Loetscher <Lukas@Loetscher.swiss>
2025-05-26 20:37:59 +0200

    fixed setting to false instead of true for keylistener

--- src/main/java/myEngine/KeyListener.java
+++ src/main/java/myEngine/KeyListener.java
@@ -1,7 +1,8 @@
 package myEngine;
 
-import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
-import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+import java.util.Arrays;
+
+import static org.lwjgl.glfw.GLFW.*;
 
 public class KeyListener {
     private static final KeyListener instance = new KeyListener();
@@ -15,7 +16,7 @@ public class KeyListener {
     }
     public static void keyCallback(long window, int key, int scancode, int action, int mods){
         if(action == GLFW_PRESS){
-            get().keyPressed[key] = false;   //I think we do not need an error handling here. handling the error for when an int larger than 350 is sent would be teh same error anyway as the one thrown by the Array  natively,
+            get().keyPressed[key] = true;   //I think we do not need an error handling here. handling the error for when an int larger than 350 is sent would be teh same error anyway as the one thrown by the Array  natively,
         }else if (action == GLFW_RELEASE){
             get().keyPressed[key] = false;
         }
@@ -23,5 +24,9 @@ public class KeyListener {
 
     public static boolean isKeyPressed(int keyCode){
         return get().keyPressed[keyCode];
+    }
+
+    public static void debugPrintKeys(){
+        System.out.println(Arrays.toString(get().keyPressed));
     }
 }
