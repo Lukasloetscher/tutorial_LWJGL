7e1ba04863063ca657f46af3100fc7472cbe25f7
Lukas Loetscher <Lukas@Loetscher.swiss>
2025-06-01 17:47:03 +0200

    initialised a square

--- src/main/java/myEngine/LevelEditorScene.java
+++ src/main/java/myEngine/LevelEditorScene.java
@@ -1,6 +1,12 @@
 package myEngine;
 
+import org.lwjgl.BufferUtils;
+
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
 import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
 
 public class LevelEditorScene extends Scene {
 
@@ -33,7 +39,19 @@ public class LevelEditorScene extends Scene {
 
     private int vertexShaderID, fragmentShaderID, shaderProgramID;
 
-
+    private float[] vertexArray = {
+            //position                  //color
+            0.5f, -0.5f, 0.0f,          1.0f,0.0f,0.0f,1.0f, //Bottom right
+            -0.5f, 0.5f, 0.0f,          0.0f,0.0f,0.0f,1.0f, //Top left
+            0.5f, 0.5f, 0.0f,           0.0f,0.0f,1.0f,1.0f, //Top right
+            -0.5f, -0.5f, 0.0f,         1.0f,1.0f,0.0f,1.0f //Bottom left
+    };
+    //IMPORTANT THIS MUST BE IN COUNTER-CLOCKWISE ORDER!
+    private int[] elementArray = {
+            0,2,1,
+            0,1,3
+    };
+    private int vaoID, vboID, eboID;
 
     public LevelEditorScene() {
 
@@ -99,6 +117,43 @@ public class LevelEditorScene extends Scene {
             System.out.println(glGetProgramInfoLog(vertexShaderID, len));
             throw new RuntimeException();
         }
+
+        //Generate Buffer objects
+
+        vaoID = glGenVertexArrays();
+        glBindVertexArray(vaoID);
+
+        //Create a float buffer of vertices;
+        FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(vertexArray.length);
+        vertexBuffer.put(vertexArray).flip();
+
+        //Create VBO upload the vertex Buffer
+        vboID = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW);
+
+        //create the indices and upload;
+        IntBuffer elementBuffer = BufferUtils.createIntBuffer(elementArray.length);
+        elementBuffer.put(elementArray).flip();
+
+        eboID = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_STATIC_DRAW);
+
+        //Add the vertex Attributes Pointers
+
+        int positionSize  = 3;
+        int colorSize = 4;
+        int floatSizeBytes = 4;
+        int vertexSizeInBytes = (positionSize + colorSize) + floatSizeBytes;
+        glVertexAttribPointer(0,positionSize,GL_FLOAT, false, vertexSizeInBytes,0);  //The first 0 corresponds to layout (location=0) in vec3 aPos;
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1,colorSize,GL_FLOAT, false, vertexSizeInBytes,positionSize * floatSizeBytes);  //The first 1 corresponds to layout (location=1) in vec4 aColor;
+                                                                                                                                    // The 0 resp position Size in the last field is the offset (in Bytes)
+        glEnableVertexAttribArray(1);
+
+
+
 
 
     }
